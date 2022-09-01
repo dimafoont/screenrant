861330217d805beadbf0af22ab15ff395f0b2382
dimafoont <foontee4ek@gmail.com>
2022-09-01 20:33:04 +0300

    DEBUG True final

--- landscape/settings.py
+++ landscape/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2^q%$df)#ds8f8$c^z*r5j&d0as$mwg14zk@se^4%uvnga)g)7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['167.172.135.10']
 
