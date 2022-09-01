7f4911c7ae76fdef34a15479af88b731c9798dab
dimafoont <foontee4ek@gmail.com>
2022-09-01 18:41:38 +0300

    DB settings

--- landscape/settings.py
+++ landscape/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'landscape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'landscapeDB',
+            'USER': 'landscapeuser',
+            'PASSWORD': 'foont160395',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
