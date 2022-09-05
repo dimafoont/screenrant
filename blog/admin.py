b7d333ebd55f5b53e38bc8cbf19c0d2dc9b5e6e6
dimafoont <foontee4ek@gmail.com>
2022-09-06 01:40:51 +0300

    adding created_at editable 3

--- blog/admin.py
+++ blog/admin.py
@@ -26,7 +26,7 @@ class PostAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
     list_display_links = ('title',)
-    list_editable = ('created_at',)
+    list_editable = ('created_at', 'category')
     list_filter = ('category',)
     readonly_fields = ('views', 'get_photo')
     fields = ('title', 'slug', 'category', 'content', 'photo', 'get_photo', 'views', 'created_at',)
