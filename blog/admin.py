564421cbd37dd7f87a6e8179086c3dcc35c22d12
dimafoont <foontee4ek@gmail.com>
2022-09-06 14:00:22 +0300

    id fix 4

--- blog/admin.py
+++ blog/admin.py
@@ -26,10 +26,9 @@ class PostAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
     list_display_links = ('title',)
-
     list_filter = ('category',)
     readonly_fields = ('views', 'created_at', 'get_photo')
-    fields = ('title', 'slug', 'category', 'content', 'photo', 'get_photo', 'views', 'created_at',)
+    fields = ('id','title', 'slug', 'category', 'content', 'photo', 'get_photo', 'views', 'created_at',)
 
     def get_photo(self, obj):
         if obj.photo:
